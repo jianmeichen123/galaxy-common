a9c1f720b3facf00d70c6cf1979b15869ba9ec2b
zhangchunyuan <chunhuanzhang@galaxyinternet.com>
2016-02-29 17:19:12 +0800

    解决冲突

--- src/main/java/com/galaxyinternet/framework/core/filter/LoginFilter.java
+++ src/main/java/com/galaxyinternet/framework/core/filter/LoginFilter.java
@@ -15,12 +15,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
-
 import com.galaxyinternet.framework.cache.Cache;
+import com.galaxyinternet.framework.core.model.BaseUser;
 import com.galaxyinternet.framework.core.model.ResponseData;
 import com.galaxyinternet.framework.core.model.Result;
 import com.galaxyinternet.framework.core.model.Result.Status;
-import com.galaxyinternet.framework.core.model.BaseUser;
 import com.galaxyinternet.framework.core.utils.GSONUtil;
 import com.galaxyinternet.framework.core.utils.StringEx;
 
@@ -34,7 +33,7 @@ public class LoginFilter implements Filter {
 	/**
 	 * 允许游客状态的接口
 	 */
-	static String[] webExcludedUrl = {};
+	static String[] webExcludedUrl = { "/galaxy/userlogin/login" };
 
 	@Override
 	public void destroy() {
@@ -70,10 +69,21 @@ public class LoginFilter implements Filter {
 		return user;
 	}
 
+	/**
+	 * 去掉对资源文件的拦截
+	 */
+	public boolean judgeFile(String url) {
+		if (url.endsWith(".gif") || url.endsWith(".jpg") || url.endsWith(".png") || url.endsWith(".bmp")
+				|| url.endsWith(".css") || url.endsWith(".js") || url.endsWith(".jsx")) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
 	@SuppressWarnings("rawtypes")
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-
 		HttpServletRequest req = (HttpServletRequest) request;
 		BaseUser user = getUser(req);
 		if (null != user && user.getId() > 0) {
@@ -82,24 +92,30 @@ public class LoginFilter implements Filter {
 
 		String url = req.getRequestURI();
 		boolean loginFlag = true;
+
+		// 如果url是资源文件请求地址 直接放行
+		loginFlag = judgeFile(url);
+		if (!loginFlag) {
+			chain.doFilter(request, response);
+			return;
+		}
+
 		for (String excludedUrl : excludedUrlArray) {
 			if (url.contains(StringEx.replaceSpecial(excludedUrl))) {
 				loginFlag = false;
 				break;
 			}
 		}
-
 		for (String excludedUrl : webExcludedUrl) {
 			if (url.contains(excludedUrl)) {
 				loginFlag = false;
 				break;
 			}
 		}
-
 		if (loginFlag && null == user) {
-			String errorMessage = "用户未登录或会话已过期,请重新登录";
-			logger.warn(errorMessage);
+			logger.warn("用户长时间未操作或已过期");
 			response.setCharacterEncoding("utf-8");
+			String errorMessage = "用户长时间未操作或已过期,请重新登录";
 			ResponseData resposeData = new ResponseData();
 			Result result = new Result();
 			result.setStatus(Status.ERROR);
@@ -107,10 +123,8 @@ public class LoginFilter implements Filter {
 			resposeData.setResult(result);
 			response.getWriter().write(GSONUtil.toJson(resposeData));
 			return;
-		} else {
-			chain.doFilter(req, response);
 		}
-
+		chain.doFilter(req, response);
 	}
 
 	@Override
