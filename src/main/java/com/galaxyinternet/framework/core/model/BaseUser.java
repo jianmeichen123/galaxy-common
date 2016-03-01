fca9a0f988fca8641ffb41106b30f9433a89ad01
zhangchunyuan <chunhuanzhang@galaxyinternet.com>
2016-03-01 09:05:25 +0800

    BaseUser中添加了sessionId字段

--- src/main/java/com/galaxyinternet/framework/core/model/BaseUser.java
+++ src/main/java/com/galaxyinternet/framework/core/model/BaseUser.java
@@ -2,4 +2,21 @@ package com.galaxyinternet.framework.core.model;
 
 public class BaseUser  extends BaseEntity{
 	private static final long serialVersionUID = 1L;
+	
+	private String sessionId;
+
+	/**
+	 * @return the sessionId
+	 */
+	public String getSessionId() {
+		return sessionId;
+	}
+
+	/**
+	 * @param sessionId the sessionId to set
+	 */
+	public void setSessionId(String sessionId) {
+		this.sessionId = sessionId;
+	}
+	
 }
