1a92c8686ad0d9f11fafd63cf9925ef7b2f28b70
xingkaihu <kaihuxing@galaxyinternet.com>
2016-03-01 20:18:53 +0800

    添加常量信息

--- src/main/java/com/galaxyinternet/framework/core/constants/Constants.java
+++ src/main/java/com/galaxyinternet/framework/core/constants/Constants.java
@@ -1,17 +1,25 @@
 package com.galaxyinternet.framework.core.constants;
 
 public class Constants {
-	//注册时候与ssoserver通讯时加密解密的密钥
-    public final static String SECRET_KEY = "2B4eWxHdTaWPRGnN";
-    
-    public final static String SESSION_USER_KEY="galax_session_user";
-    
-    public final static String SESSION_ID_KEY="sessionId";
-    
-    public final static String SESSOPM_SID_KEY="sid";
-    
-    public final static String LOGIN_TOLOGIN ="/galaxy/userlogin/toLogin";
-    
-    public final static String LOGIN_CHECKLOGIN ="/galaxy/userlogin/login";
-    
+	// 注册时候与ssoserver通讯时加密解密的密钥
+	public final static String SECRET_KEY = "2B4eWxHdTaWPRGnN";
+
+	public final static String SESSION_USER_KEY = "galax_session_user";
+
+	public final static String SESSION_ID_KEY = "sessionId";
+
+	public final static String SESSOPM_SID_KEY = "sid";
+
+	public final static String LOGIN_TOLOGIN = "/galaxy/userlogin/toLogin";
+
+	public final static String LOGIN_CHECKLOGIN = "/galaxy/userlogin/login";
+
+	public final static String OSS_ENDPOINT_KEY = "oss.service.endpoint";
+
+	public final static String OSS_ACCESS_KEY = "oss.access.key.id";
+
+	public final static String OSS_ACCESS_KEY_SECRET = "oss.access.key.secret";
+
+	public final static String OSS_CONFIG_FILE = "oss-config.properties";
+
 }
