00e3dc1469346fd262bb4747190d45293889ede7
zhangchunyuan <chunhuanzhang@galaxyinternet.com>
2016-03-08 15:36:19 +0800

    模糊查询sql问题

--- src/main/java/com/galaxyinternet/framework/core/model/BaseEntity.java
+++ src/main/java/com/galaxyinternet/framework/core/model/BaseEntity.java
@@ -1,5 +1,10 @@
 package com.galaxyinternet.framework.core.model;
 
+import org.apache.commons.lang.StringUtils;
+import org.springframework.data.annotation.Transient;
+
+import com.galaxyinternet.framework.core.utils.StringEx;
+
 /**
  * @author kerfer
  */
@@ -9,8 +14,40 @@ public abstract class BaseEntity extends PrimaryKeyObject<Long>{
 	/**
 	 * 是否包含转义字符
 	 */
-	protected boolean escapeChar;
+	@Transient
+	protected Boolean escapeChar;
 	
+	/**
+	 * 模糊查询关键字
+	 */
+	protected String keyword;
+	
+	/**
+	 * @return the keyword
+	 */
+	public String getKeyword() {
+		if(escapeChar == null){
+			escapeChar = false;
+		}
+		if(StringUtils.isNotEmpty(keyword)&&escapeChar==false){
+			String newkeyword = StringEx.checkSql(keyword);
+			if(!keyword.equals(newkeyword)){
+				escapeChar = true;
+				keyword = newkeyword;
+			}
+			this.setEscapeChar(escapeChar);
+			System.out.println("-------escapeChar:"+escapeChar);
+		}
+		return keyword;
+	}
+
+	/**
+	 * @param keyword the keyword to set
+	 */
+	public void setKeyword(String keyword) {
+		this.keyword = keyword;
+	}
+
 	@Override
 	public Long getCreatedTime() {
 		return createdTime;
@@ -41,11 +78,11 @@ public abstract class BaseEntity extends PrimaryKeyObject<Long>{
 		this.id = id;
 	}
 
-	public boolean isEscapeChar() {
+	public Boolean getEscapeChar() {
 		return escapeChar;
 	}
 
-	public void setEscapeChar(boolean escapeChar) {
+	public void setEscapeChar(Boolean escapeChar) {
 		this.escapeChar = escapeChar;
 	}
 }
