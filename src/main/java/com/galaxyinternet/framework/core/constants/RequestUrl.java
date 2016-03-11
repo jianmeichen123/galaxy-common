eae88f60e9014a5677332906c3ed9072fe880c4b
chenjianmei <jianmeichen@galaxyinternet.com>
2016-03-11 11:03:33 +0800

    添加动态路径处理任务

--- src/main/java/com/galaxyinternet/framework/core/constants/RequestUrl.java
+++ src/main/java/com/galaxyinternet/framework/core/constants/RequestUrl.java
@@ -3,5 +3,6 @@ package com.galaxyinternet.framework.core.constants;
 public class RequestUrl {
 	
 	public final static String MENU_SOPTASK = "/galaxy/soptask";
+	public final static String DOTASK_URL = "http://fx.qa.galaxyinternet.com/sop";
 
 }
