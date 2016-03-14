834ca99e9fb43c19ee1100ebed7ef5a6f6624e8a
xingkaihu <kaihuxing@galaxyinternet.com>
2016-03-14 15:39:10 +0800

    回滚loginfilter

--- src/main/java/com/galaxyinternet/framework/core/filter/LoginFilter.java
+++ src/main/java/com/galaxyinternet/framework/core/filter/LoginFilter.java
@@ -11,7 +11,6 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
@@ -32,72 +31,89 @@ import com.galaxyinternet.framework.core.utils.StringEx;
 public class LoginFilter implements Filter {
 	private Logger logger = LoggerFactory.getLogger(LoginFilter.class);
 
-	/**
-	 * 任何情况都不需要登录，在web.xml里面配置
-	 */
 	static String[] excludedUrlArray = {};
 
-	/**
-	 * 允许游客状态的接口
-	 */
+	static Cache cache;
+
 	static String[] webExcludedUrl = { Constants.LOGIN_TOLOGIN, Constants.LOGIN_CHECKLOGIN };
 
 	@Override
 	public void destroy() {
 	}
 
-	/**
-	 * 
-	 * 请求参数完整性校验
-	 */
-	@SuppressWarnings({ "rawtypes", "unused" })
-	private String checkRequestParamValid(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		String sessionId = getSessionId(request);
-		String userId = getUserId(request);
-		if (StringUtils.isBlank(userId) || StringUtils.isBlank(sessionId)) {
-			logger.warn("请求参数不完整：userId=" + userId + "sessionId=" + sessionId);
-			response.setCharacterEncoding("utf-8");
-			ResponseData resposeData = new ResponseData();
-			Result result = new Result();
-			result.setStatus(Status.ERROR);
-			result.setMessage("请求参数不完整");
-			result.setErrorCode(Constants.REQUEST_PARAMS_INCOMPLETE);
-			resposeData.setResult(result);
-			response.getWriter().write(GSONUtil.toJson(resposeData));
-			return null;
+	private BaseUser getUser(HttpServletRequest request) {
+/*		String sessionId = request.getHeader(Constants.SESSION_ID_KEY);
+		if (StringUtils.isBlank(sessionId)) {
+			sessionId = request.getParameter(Constants.SESSOPM_SID_KEY);
 		}
-		return sessionId;
-	}
+		if (StringUtils.isNotBlank(sessionId)) {
+			BaseUser user =  getUser(request, sessionId);
+			if(null == user){
+				request.getSession().removeAttribute(Constants.SESSION_USER_KEY);
+			}else{
+				return user;
+			}
+		} 
+		return null;*/
 
-	private String getSessionId(HttpServletRequest request){
+		
+		Object userObj = request.getSession().getAttribute(Constants.SESSION_USER_KEY);
 		String sessionId = request.getHeader(Constants.SESSION_ID_KEY);
 		if (StringUtils.isBlank(sessionId)) {
 			sessionId = request.getParameter(Constants.SESSOPM_SID_KEY);
 		}
-		return sessionId;
-	}
-	
-	private String getUserId(HttpServletRequest request){
-		String userId = request.getHeader(Constants.REQUEST_HEADER_USER_ID_KEY);
-		if (StringUtils.isBlank(userId)) {
-			userId = request.getParameter(Constants.REQUEST_URL_USER_ID_KEY);
+		if (StringUtils.isNotBlank(sessionId)) {
+			BaseUser user = (BaseUser) cache.getByRedis(sessionId);
+			if(user==null){
+				request.getSession().removeAttribute(Constants.SESSION_USER_KEY);
+				return null;
+			}else{
+				request.getSession().setAttribute(Constants.SESSION_USER_KEY, user);
+				cache.setByRedis(sessionId, user, 60 * 60 * 24 * 1);
+				return user;
+			}
+		}else{
+			if(userObj==null){
+				return null;
+			}
+			return (BaseUser) userObj;
 		}
-		return userId;
-	}
-	
-	private BaseUser getUser(HttpServletRequest request) {
-		String sessionId = getSessionId(request);
-		if (StringUtils.isBlank(sessionId))
-			return null;
+		/*	
 		Object userObj = request.getSession().getAttribute(Constants.SESSION_USER_KEY);
 		if (userObj == null) {
-			return null;
+			String sessionId = request.getHeader(Constants.SESSION_ID_KEY);
+			if (StringUtils.isBlank(sessionId)) {
+				sessionId = request.getParameter(Constants.SESSOPM_SID_KEY);
+			}
+			if (StringUtils.isNotBlank(sessionId)) {
+				return getUser(request, sessionId);
+			} else {
+				return null;
+			}
 		}
-		return (BaseUser) userObj;
+		return (BaseUser) userObj;*/
 	}
 
 	/**
-	 * 去掉对资源文件的拦截
+	 * 鑾峰彇鐢ㄦ埛淇℃伅
+	 * 
+	 * @param request
+	 *            request
+	 * @param key
+	 *            sessionId key
+	 * @return user
+	 */
+	@SuppressWarnings("unused")
+	private BaseUser getUser(HttpServletRequest request, String key) {
+		BaseUser user = (BaseUser) cache.getByRedis(key);
+		if (user != null) {
+			cache.setByRedis(key, user, 60 * 60 * 24 * 1);
+		}
+		return user;
+	}
+
+	/**
+	 * 鍘绘帀瀵硅祫婧愭枃浠剁殑鎷︽埅
 	 */
 	public boolean judgeFile(String url) {
 		if (url.endsWith(".gif") || url.endsWith(".jpg") || url.endsWith(".png") || url.endsWith(".bmp")
@@ -108,41 +124,38 @@ public class LoginFilter implements Filter {
 		}
 	}
 
-	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
+	@SuppressWarnings("rawtypes")
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		HttpServletRequest request = (HttpServletRequest) req;
-		HttpServletResponse response = (HttpServletResponse) resp;
-		/**
-		 * 验证请求参数的合法性
-		 */
-		// checkRequestParamValid(request, response);
-
-		BaseUser user = getUser(request);
+		HttpServletRequest req = (HttpServletRequest) request;
+		BaseUser user = getUser(req);
 		if (null != user && user.getId() > 0) {
-			request.getSession().setAttribute(Constants.SESSION_USER_KEY, user);
+			req.getSession().setAttribute(Constants.SESSION_USER_KEY, user);
 		}
 
-		String url = request.getRequestURI();
-		// 如果url是资源文件请求地址 直接放行
-		if (!judgeFile(url)) {
+		String url = req.getRequestURI();
+		boolean loginFlag = true;
+
+		// 濡傛灉url鏄祫婧愭枃浠惰姹傚湴鍧�鐩存帴鏀捐
+		loginFlag = judgeFile(url);
+		if (!loginFlag) {
 			chain.doFilter(request, response);
 			return;
 		}
 
 		for (String excludedUrl : excludedUrlArray) {
 			if (url.contains(StringEx.replaceSpecial(excludedUrl))) {
-				chain.doFilter(request, response);
-				return;
+				loginFlag = false;
+				break;
 			}
 		}
 		for (String excludedUrl : webExcludedUrl) {
 			if (url.contains(excludedUrl)) {
-				chain.doFilter(request, response);
-				return;
+				loginFlag = false;
+				break;
 			}
 		}
-
-		/*if (null == user) {
+		if (loginFlag && null == user) {
 			logger.warn("用户长时间未操作或已过期");
 			response.setCharacterEncoding("utf-8");
 			String errorMessage = "用户长时间未操作或已过期,请重新登录";
@@ -154,8 +167,7 @@ public class LoginFilter implements Filter {
 			resposeData.setResult(result);
 			response.getWriter().write(GSONUtil.toJson(resposeData));
 			return;
-		}*/
-
+		}
 		chain.doFilter(req, response);
 	}
 
@@ -167,7 +179,7 @@ public class LoginFilter implements Filter {
 		}
 		ServletContext servletContext = config.getServletContext();
 		WebApplicationContext wac = WebApplicationContextUtils.getWebApplicationContext(servletContext);
-		Cache cache = (Cache) wac.getBean(Constants.REDIS_CACHE_BEAN_NAME);
+		cache = (Cache) wac.getBean("cache");
 		@SuppressWarnings("unchecked")
 		Map<String, Object> configs = (Map<String, Object>) cache.get(OSSConstant.GALAXYINTERNET_FX_ENDPOINT);
 		servletContext.setAttribute(OSSConstant.GALAXYINTERNET_FX_ENDPOINT, GSONUtil.toJson(configs));
