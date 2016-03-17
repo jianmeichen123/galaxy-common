447090d6a127c75df46edd97112f8ce5d57e4220
xingkaihu <kaihuxing@galaxyinternet.com>
2016-03-17 16:42:08 +0800

    添加描述

--- src/main/java/com/galaxyinternet/framework/core/config/PlaceholderConfigurer.java
+++ src/main/java/com/galaxyinternet/framework/core/config/PlaceholderConfigurer.java
@@ -9,6 +9,9 @@ public class PlaceholderConfigurer {
 	 * @param args
 	 *            替换占位符的参数
 	 * @return 替换后的字符串
+	 * 
+	 * @toUse PlaceholderConfigurer.formatText("你好%s,环境来到%s系统","keifer", "繁星");
+	 * 
 	 */
 	public static String formatText(String string, Object... args) {
 		return String.format(string, args);
