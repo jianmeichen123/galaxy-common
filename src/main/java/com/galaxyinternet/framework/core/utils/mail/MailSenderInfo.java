decdf876aea36376699fb6a84adbd98463fca298
zhaoying <yingzhao@galaxyunternet.com>
2016-03-17 15:03:03 +0800

    --zhaoying--发送邮件增加tls

--- src/main/java/com/galaxyinternet/framework/core/utils/mail/MailSenderInfo.java
+++ src/main/java/com/galaxyinternet/framework/core/utils/mail/MailSenderInfo.java
@@ -19,16 +19,18 @@ import com.galaxyinternet.framework.core.utils.PropertiesUtils;
  * 发送邮件需要使用的基本信息 此处需要修改
  */
 public class MailSenderInfo {
+	
 	 // 发送邮件的服务器的IP和端口
-    private String mailServerHost = ""; // 默认服务器
-    private String mailServerPort = "";
+    private String mailServerHost = "mail.galaxyinternet.com"; // 默认服务器
+    private String mailServerPort = "587";    
+    // 邮件发送者的地址    
     // 邮件发送者的地址
-    private String fromAddress = "";
+    private String fromAddress = "fx-admin@galaxyinternet.com";
     // 邮件接收者的地址
     private String toAddress;
     // 登陆邮件发送服务器的用户名和密码
-    private String userName = "";
-    private String password = "";
+    private String userName = "fx-admin";
+    private String password = "oN1oAyt4";
     // 是否需要身份验证
     private boolean validate = true;
 	// 邮件主题
@@ -47,7 +49,7 @@ public class MailSenderInfo {
 	 * 获得邮件会话属性
 	 */
 	public MailSenderInfo() {
-		Properties property = PropertiesUtils.getProperties(Constants.MAIL_CONFIG_FILE);
+	    Properties property = PropertiesUtils.getProperties(Constants.MAIL_CONFIG_FILE);
 		mailServerHost = property.getProperty(Constants.MAIL_HOST_KEY);
 		mailServerPort = property.getProperty(Constants.MAIL_SMTP_PORT_KEY);
 		fromAddress = property.getProperty(Constants.MAIL_ADDRESS_KEY);
@@ -58,10 +60,16 @@ public class MailSenderInfo {
 	}
 
 	public Properties getProperties() {
+		
 		Properties p = new Properties();
 		p.put("mail.smtp.host", this.mailServerHost);
 		p.put("mail.smtp.port", this.mailServerPort);
 		p.put("mail.smtp.auth", validate ? "true" : "false");
+		//以下配置支持tls
+		p.put("mail.smtp.starttls.enable", "true");
+		p.put("mail.smtp.ssl.checkserveridentity", "false"); 
+		p.put("mail.smtp.ssl.trust", "mail.galaxyinternet.com");  
+
 		return p;
 	}
 
