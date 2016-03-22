a15b12c217c1a8ed9bb5332722a9375e34df2b4a
zhangchunyuan <chunhuanzhang@galaxyinternet.com>
2016-03-22 10:41:06 +0800

    修改loginfiter

--- src/main/java/com/galaxyinternet/framework/core/filter/LoginFilter.java
+++ src/main/java/com/galaxyinternet/framework/core/filter/LoginFilter.java
@@ -40,7 +40,7 @@ public class LoginFilter implements Filter {
 	/**
 	 * 允许游客状态的接口
 	 */
-	static String[] webExcludedUrl = { Constants.LOGIN_TOLOGIN, Constants.LOGIN_CHECKLOGIN, Constants.SOP_REDIRECT };
+	static String[] webExcludedUrl = { Constants.LOGIN_TOLOGIN, Constants.LOGIN_CHECKLOGIN };
 
 	@Override
 	public void destroy() {
