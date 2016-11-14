2d69d03aa6a06c4718f3ef98b05e55f9d134375f
zhongliangzhang <zhongliangzhang@galaxyinternet.com>
2016-11-14 18:07:02 +0800

    放假放假

--- src/main/java/com/galaxyinternet/framework/core/model/PrimaryKeyObject.java
+++ src/main/java/com/galaxyinternet/framework/core/model/PrimaryKeyObject.java
@@ -25,7 +25,28 @@ public abstract class PrimaryKeyObject<PK extends Serializable> implements Seria
 	 * 更新时间
 	 */
 	protected Long updatedTime;
-
+	
+	/**
+	 * 是否有效
+	 */
+	protected String isValid;
+	
+	/**
+	 * 排序
+	 */
+	protected Long sortNum;
+	
+	/**
+	 * 版本
+	 */
+	protected String version;
+	
+	/**
+	 * 备注
+	 */
+	protected String memo;
+	
+	
 	public abstract Long getCreatedTime();
 
 	public abstract void setCreatedTime(Long createdTime);
@@ -33,5 +54,43 @@ public abstract class PrimaryKeyObject<PK extends Serializable> implements Seria
 	public abstract Long getUpdatedTime();
 
 	public abstract void setUpdatedTime(Long updatedTime);
+
+	public String getIsValid() {
+		return isValid;
+	}
+
+	public void setIsValid(String isValid) {
+		this.isValid = isValid;
+	}
+
+	public Long getSortNum() {
+		return sortNum;
+	}
+
+	public void setSortNum(Long sortNum) {
+		this.sortNum = sortNum;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+	public String getMemo() {
+		return memo;
+	}
+
+	public void setMemo(String memo) {
+		this.memo = memo;
+	}
+	
+	
+	
+	
+	
+	
 
 }
