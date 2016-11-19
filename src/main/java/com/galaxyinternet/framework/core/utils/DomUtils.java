c77d779b886cd7442f7074196ee072ee48df4bbc
swang <songwang@galaxyinternet.com>
2016-11-19 17:04:28 +0800

    dom

--- src/main/java/com/galaxyinternet/framework/core/utils/DomUtils.java
+++ src/main/java/com/galaxyinternet/framework/core/utils/DomUtils.java
@@ -1,8 +1,11 @@
 package com.galaxyinternet.framework.core.utils;
 
+import java.util.List;
+
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
 import org.dom4j.Node;
 
 public class DomUtils
@@ -20,11 +23,11 @@ public class DomUtils
 		}
 		return null;
 	}
-	public static String getNodeValue(Document doc, String path)
+	public static String getNodeValue(Element ele, String path)
 	{
 		try
 		{
-			Node node = doc.selectSingleNode(path);
+			Node node = ele.selectSingleNode(path);
 			if(node != null)
 			{
 				return node.getText();
@@ -47,6 +50,35 @@ public class DomUtils
 			{
 				return node.getText();
 			}
+		}
+		catch (DocumentException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	public static List<?> getNodeList(Element ele, String path)
+	{
+		
+		try
+		{
+			return ele.selectNodes(path);
+		}
+		catch (Exception e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	public static List<?> getNodeList(String xml, String path)
+	{
+		
+		try
+		{
+			Document doc = DocumentHelper.parseText(xml); 
+			return doc.selectNodes(path);
 		}
 		catch (DocumentException e)
 		{
